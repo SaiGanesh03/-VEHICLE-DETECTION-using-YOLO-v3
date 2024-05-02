1517916eac6c348273ad5f8eca7b793129e0e9ac
Sai Ganesh A <103567605+SaiGanesh03@users.noreply.github.com>
2024-05-02 16:27:50 +0530

    Add files via upload

---
+++ for_video.py
@@ -0,0 +1,192 @@
+import cv2
+import csv
+import collections
+import numpy as np
+from tracker import *
+
+# Initialize Tracker
+tracker = EuclideanDistTracker()
+
+# Initialize the videocapture object
+cap = cv2.VideoCapture("project_video.mp4")
+input_size = 320
+
+# Detection confidence threshold
+confThreshold = 0.2
+nmsThreshold = 0.2
+
+font_color = (0, 0, 255)
+font_size = 0.5
+font_thickness = 2
+
+# Middle cross line position
+middle_line_position = 225
+up_line_position = middle_line_position - 15
+down_line_position = middle_line_position + 15
+
+# Store Coco Names in a list
+classesFile = "coco.names"
+classNames = open(classesFile).read().strip().split('\n')
+print(classNames)
+print(len(classNames))
+
+# class index for our required detection classes
+required_class_index = [2, 3, 5, 7]
+
+detected_classNames = []
+
+## Model Files
+modelConfiguration = 'yolov3-320.cfg'
+modelWeights = 'yolov3-320.weights'
+
+# configure the network model
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+# Configure the network backend
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# Define random colour for each class
+np.random.seed(42)
+colors = np.random.randint(0, 255, size=(len(classNames), 3), dtype='uint8')
+
+
+# Function for finding the center of a rectangle
+def find_center(x, y, w, h):
+    x1 = int(w / 2)
+    y1 = int(h / 2)
+    cx = x + x1
+    cy = y + y1
+    return cx, cy
+
+
+# List for store vehicle count information
+temp_up_list = []
+temp_down_list = []
+up_list = [0, 0, 0, 0]
+down_list = [0, 0, 0, 0]
+
+
+# Function for count vehicle
+def count_vehicle(box_id, img):
+    x, y, w, h, id, index = box_id
+
+    # Find the center of the rectangle for detection
+    center = find_center(x, y, w, h)
+    ix, iy = center
+
+    # Find the current position of the vehicle
+    if (iy > up_line_position) and (iy < middle_line_position):
+        if id not in temp_up_list:
+            temp_up_list.append(id)
+    elif iy < down_line_position and iy > middle_line_position:
+        if id not in temp_down_list:
+            temp_down_list.append(id)
+    elif iy < up_line_position:
+        if id in temp_down_list:
+            temp_down_list.remove(id)
+            up_list[index] = up_list[index] + 1
+    elif iy > down_line_position:
+        if id in temp_up_list:
+            temp_up_list.remove(id)
+            down_list[index] = down_list[index] + 1
+
+    # Draw circle in the middle of the rectangle
+    cv2.circle(img, center, 2, (0, 0, 255), -1)
+
+
+# Function for finding the detected objects from the network output
+import numpy as np
+
+def postProcess(outputs, img):
+    global detected_classNames
+    height, width = img.shape[:2]
+    boxes = []
+    classIds = []
+    confidence_scores = []
+    detection = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if classId in required_class_index:
+                if confidence > confThreshold:
+                    w, h = int(det[2] * width), int(det[3] * height)
+                    x, y = int((det[0] * width) - w / 2), int((det[1] * height) - h / 2)
+                    boxes.append([x, y, w, h])
+                    classIds.append(classId)
+                    confidence_scores.append(float(confidence))
+
+    # Apply Non-Max Suppression
+    indices = cv2.dnn.NMSBoxes(boxes, confidence_scores, confThreshold, nmsThreshold)
+    indices = np.array(indices)  # Convert to NumPy array
+    for i in indices.flatten():
+        x, y, w, h = boxes[i]
+        color = [int(c) for c in colors[classIds[i]]]
+        name = classNames[classIds[i]]
+        detected_classNames.append(name)
+        # Draw classname and confidence score
+        cv2.putText(img,f'{name.upper()} {int(confidence_scores[i]*100)}%',
+                  (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+        # Draw bounding rectangle
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
+        detection.append([x, y, w, h, required_class_index.index(classIds[i])])
+
+    # Update the tracker for each object
+    boxes_ids = tracker.update(detection)
+    for box_id in boxes_ids:
+        count_vehicle(box_id, img)
+
+
+
+def process_video():
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, (0, 0), None, 0.5, 0.5)
+
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255, (input_size, input_size), [0, 0, 0], 1, crop=False)
+
+        # Set the input of the network
+        net.setInput(blob)
+        output_layers = net.getUnconnectedOutLayersNames()
+
+        # Feed data to the network
+        outputs = net.forward(output_layers)
+
+        # Find the objects from the network output
+        postProcess(outputs, frame)
+
+        # Draw the crossing lines
+        cv2.line(frame, (0, middle_line_position), (frame.shape[1], middle_line_position), (255, 0, 255), 2)
+        cv2.line(frame, (0, up_line_position), (frame.shape[1], up_line_position), (0, 0, 255), 2)
+        cv2.line(frame, (0, down_line_position), (frame.shape[1], down_line_position), (0, 0, 255), 2)
+
+
+
+        # Show the frames
+        cv2.imshow('Output', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # Write the vehicle counting information in a file and save it
+    with open("data.csv", 'w') as f1:
+        cwriter = csv.writer(f1)
+        cwriter.writerow(['Direction', 'car', 'motorbike', 'bus', 'truck'])
+        up_list.insert(0, "Up")
+        down_list.insert(0, "Down")
+        cwriter.writerow(up_list)
+        cwriter.writerow(down_list)
+
+    # Finally release the capture object and destroy all active windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    process_video()
